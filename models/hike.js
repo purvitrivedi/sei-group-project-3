20630e3aa623fc321a9b7cc8902d7e158960bac3
andy8radshaw <andy8radshaw@gmail.com>
2020-05-15 10:59:44 +0100

    added hike model

--- models/hike.js
+++ models/hike.js
@@ -1,1 +1,55 @@
-console.log('hello world hike')
+const mongoose = require('mongoose')
+
+// user can add images to the hike page
+const userImagesSchema = new mongoose.Schema({
+  image: { type: String, required: true },
+  user: { type: mongoose.Schema.ObjectId, ref: 'User', required: true }
+})
+// user can review the hike
+const reviewSchema = new mongoose.Schema({
+  text: { type: String, required: true, maxlength: 1000 },
+  user: { type: mongoose.Schema.ObjectId, ref: 'User', required: true }
+}, {
+  timestamps: true
+})
+
+//user can rate the hike from 1 to 5
+const ratingSchema = new mongoose.Schema({
+  rating: { type: Number, required: true, min: 1, max: 5 }
+})
+
+
+//general hike profile
+const hikeSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  description: { type: String, required: true },
+  location: { 
+    country: { type: String, required: true },
+    lat: { type: Number, required: true },
+    lon: { type: Number, required: true }
+  },
+  difficulty: { type: String, required: true },
+  distance: { type: String, required: true },
+  timeToComplete: { type: String, required: true },
+  images: { type: Array, required: true },
+  imagesUser: [userImagesSchema],
+  reviews: [reviewSchema],
+  ratings: [ratingSchema],
+  seasons: { type: Array, required: true },
+  user: { type: mongoose.Schema.ObjectId, ref: 'User', required: true }
+}, { 
+  timestamps: true
+})
+
+
+//obtaining the list of users that have completed this hike
+hikeSchema
+  .virtual('usersCompleted', {
+    ref: 'User',
+    localField: '_id',
+    foreignField: 'completedHikes'
+  })
+
+hikeSchema.plugin(require('mongoose-unique-validator'))
+
+module.exports = mongoose.model('Hike', hikeSchema)
