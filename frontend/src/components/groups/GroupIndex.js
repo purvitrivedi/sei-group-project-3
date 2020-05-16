c8d62ecf40ff8a34f7cc44a969adfcf8d4729bd4
Kuriko IWAI <kuriko10091009@gmail.com>
2020-05-17 03:04:50 +0900

    minor change

--- frontend/src/components/groups/GroupIndex.js
+++ frontend/src/components/groups/GroupIndex.js
@@ -1,32 +1,48 @@
 import React from 'react'
-// import axios from 'axios'
-// import { getAllCheeses } from '../../lib/api'
+import axios from 'axios'
+import GroupCard from './GroupCard'
+// import { getAllGroups } from '../../lib/api'
 
 class GroupIndex extends React.Component {
-  // state = { 
-  //   groups: [] 
-  // }
+  state = { 
+    groups: [] 
+  }
 
-  // async componentDidMount() {
-  //   try {
-  //     const res = await axios.get('/groups')
-  //     this.setState({ groups: res.data })
-  //   } catch (err) {
-  //     this.props.history.push('/notfound')
-  //   }
-  // }
+  async componentDidMount() {
+    try {
+      const res = await axios.get('/api/groups')
+      this.setState({ groups: res.data })
+    } catch (err) {
+      this.props.history.push('/notfound')
+    }
+  }
   render() {
+    console.log(this.state)
     return (
-      <h1>Groups INDEX</h1>
-      // <section className="section">
-      //   <div className="container">
-      //     <div className="columns is-multiline">
-      //       {this.state.cheeses.map(cheese => (
-      //         <CheeseCard key={cheese._id} {...cheese}/>
-      //       ))}
-      //     </div>
-      //   </div>
-      // </section>
+      <div className="GroupIndex">
+        <section className="GroupIndex hero">
+          <div className="hero-body">
+            <div className="container">
+              <h1 className="title">
+                Welcome to Group Pages
+              </h1>
+              <h2 className="subtitle">
+                Find your groups and join today!
+              </h2>
+            </div>
+          </div>
+        </section>
+
+        <section className="GroupIndex section">
+          <div className="container">
+            <div className="columns is-multiline">
+              {this.state.groups.map(group => (
+                <GroupCard key={group._id} {...group}/>
+              ))}
+            </div>
+          </div>
+        </section>
+      </div>
     )
   }
 }
