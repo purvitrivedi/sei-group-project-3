be9ad2a9e0ad0d5ecfa6f671aa1937d7b6fe85c1
purvitrivedi <purvitriv@gmail.com>
2020-05-18 23:09:26 +0100

    Profile-page-complete

--- frontend/src/components/profiles/ProfileShow.js
+++ frontend/src/components/profiles/ProfileShow.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { getToken, isOwner } from '../../lib/auth'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 
 import ProfileGroups from './ProfileGroups'
@@ -8,6 +9,8 @@ import ProfileFav from './ProfileFav'
 import ProfileComplete from './ProfileComplete'
 import AddCompletedHike from './AddCompletedHike'
 import ProfileImageUpload from './ProfileImageUpload'
+
+import defaultImage from './defaultProfilePic.png'
 
 class ProfileShow extends React.Component {
 
@@ -17,7 +20,8 @@ class ProfileShow extends React.Component {
     editTerm: 'Edit',
     showBio: true,
     bio: '',
-    image: ''
+    image: '',
+    fullName: ''
   }
 
   async componentDidMount() {
@@ -29,7 +33,7 @@ class ProfileShow extends React.Component {
         }
       }
       const res = await axios.get(`/api/profiles/${id}`, withHeaders())
-      this.setState({ profile: res.data, bio: res.data.bio, image: res.data.profileImage })
+      this.setState({ profile: res.data, bio: res.data.bio, image: res.data.profileImage, fullName: res.data.fullName })
     } catch (err) {
       console.log(err.response)
     }
@@ -85,13 +89,15 @@ class ProfileShow extends React.Component {
     this.setState({ showBio: !this.state.showBio })
   }
 
-  changeBio = e => {
-    const bio = e.target.value
-    this.setState({ bio })
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
   }
 
-  sendPutRequest = async () => {
+  sendPutRequest = async (e) => {
+    e.preventDefault()
     const bio = this.state.bio
+    const image = this.state.image
+    const fullName = this.state.fullName
     try {
       const id = this.props.match.params.id
       const withHeaders = () => {
@@ -99,10 +105,12 @@ class ProfileShow extends React.Component {
           headers: { Authorization: `Bearer ${getToken()}` }
         }
       }
-      const res = await axios.put(`/api/profiles/${id}`, { bio: bio }, withHeaders())
+      await axios.put(`/api/profiles/${id}`, { bio: bio }, withHeaders())
+      await axios.put(`/api/profiles/${id}`, { profileImage: image }, withHeaders())
+      await axios.put(`/api/profiles/${id}`, { fullName }, withHeaders())
 
       const resGet = await axios.get(`/api/profiles/${id}`, withHeaders())
-      this.setState({ profile: resGet.data, bio: resGet.data.bio, showBio: true })
+      this.setState({ profile: resGet.data, bio: resGet.data.bio, showBio: true, image: resGet.data.profileImage, fullName: resGet.data.fullName })
 
 
     } catch (err) {
@@ -111,7 +119,13 @@ class ProfileShow extends React.Component {
   }
 
   handleImageChange = (e) => {
-    this.setState({[e.target.name]: e.target.value})
+    this.setState({
+      [e.target.name]: e.target.value
+    }, () => {
+      this.sendPutRequest()
+    })
+
+
   }
 
 
@@ -119,100 +133,134 @@ class ProfileShow extends React.Component {
 
   render() {
     const { profile } = this.state
-    console.log(this.state.image)
     let completedHikes
     if (profile.completedHikes) {
-      completedHikes = profile.completedHikes.map(hike => {
-        return <ProfileComplete key={hike._id} {...hike} handleClick={this.removeCompHike} edit={this.state.edit} />
-      })
+      if (profile.completedHikes.length > 0) {
+        completedHikes = profile.completedHikes.map(hike => {
+          return <ProfileComplete key={hike._id} {...hike} handleClick={this.removeCompHike} edit={this.state.edit} />
+        })
+      } else {
+        if (isOwner(this.state.profile._id)) {
+          completedHikes = <div className="no-hikes">No new hikes yet. Why not add a new one?</div>
+        } else { completedHikes = <div className="no-hikes"> No fav hikes added</div> }
+      }
     }
     let favoritedHikes
     if (profile.favoritedHikes) {
-      favoritedHikes = profile.favoritedHikes.map(hike => {
-        return <ProfileFav key={hike._id} {...hike} edit={this.state.edit} />
-      })
+      if (profile.favoritedHikes.length > 0) {
+        favoritedHikes = profile.favoritedHikes.map(hike => {
+          return <ProfileFav key={hike._id} {...hike} edit={this.state.edit} />
+        })
+      } else {
+        if (isOwner(this.state.profile._id)) {
+          favoritedHikes = <Link to="/hikes"><div>Explore Hikes</div></Link>
+        } else { favoritedHikes = <div className="no-hikes"> No fav hikes added</div> }
+
+      }
     }
 
     let joinedGroups
     if (profile.joinedGroups) {
-      joinedGroups = profile.joinedGroups.map(groupId => (<ProfileGroups key={groupId} id={groupId} edit={this.state.edit} />))
+      if (profile.joinedGroups.length > 0) {
+        joinedGroups = profile.joinedGroups.map(groupId => (<ProfileGroups key={groupId} id={groupId} edit={this.state.edit} />))
+      } else {
+        if (isOwner(this.state.profile._id)) {
+          joinedGroups = <Link to="/groups"> <div className="no-group">Explore Hikr Groups</div></Link>
+        } else { joinedGroups = <div className="no-group"> No groups joined yet.</div> }
+
+      }
     }
     return (
       <div className="profile-show">
         <div className="tile is-ancestor">
-          <div className="tile is-vertical is-3">
-            <div className="tile">
-              <div className="tile is-parent is-vertical">
-                <figure>
-                  <img src={this.state.image} alt="profileImage" />
-                  {this.state.edit &&
-                    <div className="control">
-                      <ProfileImageUpload
-                        onChange={this.handleImageChange}
-                        name="image"
-                      />
-                    </div>
-                  }
-                  {(profile.fullName) && <p className="subtitle fullname">{profile.fullName}</p>}
-                  <p className="column">@{profile.username}</p>
-
-                  {isOwner(profile._id) && <p onClick={this.enableEdit} className="edit">{this.state.editTerm}</p>}
-                </figure>
-
-                <div className="tile is-child groups box">
-                  <article>
-                    <h1 className="subtitle">I follow...</h1>
-                    {joinedGroups}
-                  </article>
+          <div className="tile-pad">
+            <figure className="left-tile">
+              {this.state.image && <img src={this.state.image} alt="profileImage" />}
+              {!this.state.image && <img src={defaultImage} />}
+              {this.state.edit &&
+                <div className="control">
+                  <ProfileImageUpload
+                    onChange={this.handleImageChange}
+                    name="image"
+                  />
                 </div>
-
-              </div>
-            </div>
-          </div>
-
-          <div className="tile is-4">
-            <div className="is-vertical tile-pad">
-              <div className="notification is-success columns is-multiline">
-                <article className="column is-full">
-                  <h1 className="subtitle">I'd like to go to...</h1>
-                  <div className="column columns is-multiline">
-                    {favoritedHikes}
-                  </div>
-                </article>
-              </div>
-              <article className="box bio-box">
-                {this.state.edit && <p onClick={this.enableEditBio} className="edit-bio">Edit bio</p>}
-                {this.state.showBio && <div className="bio">
-                  {profile.bio}
-                </div>}
-                {!this.state.showBio &&
-                  <div className="bio">
+              }
+              {(profile.fullName) && <p className="subtitle">{profile.fullName}</p>}
+              {!(profile.fullName) && this.state.edit &&
+                <div className="name control">
+                  <form className="columns" onSubmit={this.sendPutRequest}>
                     <input
-                      className="textarea"
-                      type="textarea"
-                      value={this.state.bio}
-                      onChange={this.changeBio}
-
+                      type="text"
+                      className="input fullname column"
+                      placeholder="Add your Full name"
+                      name="fullName"
+                      value={this.state.fullName}
+                      onChange={this.handleChange}
                     />
-                    <p className="edit-bio" onClick={this.sendPutRequest}>Change</p>
-                  </div>}
+                    <button type="submit" className="column name-btn">→</button>
+                  </form>
+                </div>
+              }
+
+
+              <p className="column">@{profile.username}</p>
+
+              {isOwner(profile._id) && <p onClick={this.enableEdit} className="edit">{this.state.editTerm}</p>}
+            </figure>
+
+            <div className="tile is-child groups box">
+              <article>
+                <h1 className="subtitle">I've joined...</h1>
+                {joinedGroups}
               </article>
             </div>
+
+          </div>
+          <div className="tile-pad">
+            <div className="notification fav is-success columns is-multiline">
+              <article className="column is-full">
+                <h1 className="subtitle">I'd like to go to...</h1>
+                <div className="column columns is-multiline">
+                  {favoritedHikes}
+                </div>
+              </article>
+            </div>
+            <div className="bio-box notification columns is-multiline">
+              {/* <article> */}
+
+              <div className="subtitle">About me...</div>
+              {this.state.edit && <p onClick={this.enableEditBio} className="edit-bio">Edit bio</p>}
+              {this.state.showBio && <div className="bio">
+                <p>
+                  {profile.bio}
+                </p></div>}
+
+              {!this.state.showBio &&
+                <div>
+                  <textarea
+                    className="textarea"
+                    value={this.state.bio}
+                    onChange={this.handleChange}
+                    name="bio"
+
+                  />
+                  <p className="edit-bio-btn" onClick={this.sendPutRequest}>Submit</p>
+                </div>}
+              {/* </article> */}
+            </div>
           </div>
 
-          <div className="tile is-4">
-            <div className="tile is-parent tile-pad">
-              <div className="notification is-warning columns is-multiline">
-                <article className="column is-full">
-                  <h1 className="subtitle">Where I've been...</h1>
-                </article>
-                <div className="column columns is-multiline">
-                  {completedHikes}
-                </div>
-                {isOwner(profile._id) &&
-                  <div className="column is-full"> <AddCompletedHike id={profile._id} handleSubmit={this.addCompHike} /></div>
-                }
+          <div className="tile-pad">
+            <div className="notification comp columns is-multiline">
+              <article className="column is-full">
+                <h1 className="subtitle">Where I've been...</h1>
+              </article>
+              <div className="column columns is-multiline">
+                {completedHikes}
               </div>
+              {isOwner(profile._id) &&
+                <div className="column is-full"> <AddCompletedHike id={profile._id} handleSubmit={this.addCompHike} /></div>
+              }
             </div>
           </div>
         </div>
--- frontend/src/components/profiles/ProfilesList.js
+++ frontend/src/components/profiles/ProfilesList.js
@@ -1,21 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import defaultImage from './defaultProfilePic.png'
 
 const ProfilesList = (props) => {
   return (
     <div className="column is-full">
       <div className="columns">
-        <img src={props.profileImage} alt="profileImage" className="column is-one-quarter is-mobile" />
+        {props.profileImage && <img src={props.profileImage} alt="profileImage" className="column is-one-quarter is-mobile list-image" />}
+        {!props.profileImage && <img src={defaultImage} className="column is-one-quarter is-mobile list-image" />}
         <div className="column columns is-multiline">
           <div className="column is-full">
             <Link to={`/profiles/${props._id}`}><div className="subtitle">{props.fullName}</div></Link>
+            {!props.fullName && <Link to={`/profiles/${props._id}`}><div className="subtitle">@{props.username}</div></Link>}
           </div>
-          <div className="column is-full">{props.bio}</div>
-          {props.completedHikes.length !== 0 &&
-            <div className="column"> Hikes Completed ⛰: {props.completedHikes.length}</div>}
-        </div>
+        <div className="column is-full">{props.bio}</div>
+        {props.completedHikes.length !== 0 &&
+          <div className="column"> Hikes Completed ⛰: {props.completedHikes.length}</div>}
       </div>
     </div>
+    </div >
   )
 }
 
