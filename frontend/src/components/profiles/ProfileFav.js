6e576deaedbdc7c926b8603da9797a1296370b70
purvitrivedi <purvitriv@gmail.com>
2020-05-19 18:22:39 +0100

    linked profile page, leave groups and create group navbar

--- frontend/src/components/profiles/ProfileFav.js
+++ frontend/src/components/profiles/ProfileFav.js
@@ -1,20 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const ProfileFav = (props) => {
   const { edit } = props
+
   return (
-    <div className="column is-full fav-comp">
+    <div className="column is-full">
+      <Link to={`/hikes/${props.hike._id}`}>
       <div className="columns">
         <img className="column is-three-fifths" src={props.hike.images[0]} alt="hikeImage" />
         <p className="column">{props.hike.name}</p>
       </div>
-      {edit && <button
-        className="button remove column"
-        onClick={props.handleClick}
-        value={props._id}
-        name="favorites"
-      >Remove</button>}
-    </div>
+      </Link >
+      {
+    edit && <button
+      className="button remove column"
+      onClick={props.handleClick}
+      value={props._id}
+      name="favorites"
+    >Remove</button>
+  }
+    </div >
+      
   )
 }
 
--- frontend/src/components/profiles/ProfileGroups.js
+++ frontend/src/components/profiles/ProfileGroups.js
@@ -1,37 +1,65 @@
 import React from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
+import { getUserId } from '../../lib/auth'
 
 
 class ProfileGroups extends React.Component {
 
-    state = {
-      group: []
-    }
+  state = {
+    group: [],
+    memberId: ''
+  }
 
-    async componentDidMount() {
-      const groupId = this.props.id
-      const res = await axios.get(`/api/groups/${groupId}`)
-      this.setState({ group: res.data })
-    }
+  async componentDidMount() {
+    const groupId = this.props.id
+    const res = await axios.get(`/api/groups/${groupId}`)
+    this.setState({ group: res.data })
+  
+    this.matchMember()
+  }
 
-    render() {
-      const { group } = this.state
-      const edit = this.props.edit
-      return (
-        <div className="column is-full fav-comp">
+  matchMember = () => {
+    const userId = getUserId()
+    const members = this.state.group.members
+    let memberId
+    if (members) {
+      members.filter(member => {
+        if(member.user === userId) 
+        memberId = member._id
+        return member._id
+      })
+      this.setState({memberId})
+    }
+  }
+
+
+
+
+  render() {
+    const { group } = this.state
+    const edit = this.props.edit
+    return (
+
+      <div className="column is-full fav-comp">
+        <Link to={`/groups/${group._id}`}>
           <div className="columns">
             <img className="column is-three-fifths" src={group.headerImage} alt="hikeImage" />
             <p className="column">{group.name}</p>
           </div>
-          {edit && <button
-            className="button remove column is-full"
-          >Remove</button>}
+        </Link>
+        {edit &&
+          <button
+            className="button remove column"
+            onClick={this.props.handleClick}
+            value={group._id}
+            name={this.state.memberId}
+          >Leave Group</button>}
+      </div>
 
-
-        </div>
-      )
-    }
-
+    )
   }
+
+}
 
 export default ProfileGroups
